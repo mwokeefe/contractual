0584a42f91da88fd4b72b09c1c439f995e161da4
Michael William O'Keefe <michael.william.o'keefe@cern.ch>
2026-04-05 22:59:01 +0100

    chore: slight tidy up and add begineer Justfile

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from contractual import Int, Float, contract, Dict
 
 
 @contract
-def example(x: Int > 1, y: Float == 0, z: Dict.max_len(2)):
-    print(x, y, z)
+def example(x: Int > 1, y: Float == 0, z: Dict.max_len(3)):
+    return x, y, z
+
+
+@contract
+def multiply(x: Float > 2, y: Float > 10) -> Float > 20:
+    return x * y
 
 
 if __name__ == "__main__":
-    example(2, 0, {"x": 1, "y": 2, "z": 3})
+    print(example(2, 0, {"x": 1, "y": 2, "z": 3}))
+    print(multiply(4, 20))
